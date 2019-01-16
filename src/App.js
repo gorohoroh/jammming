0c73968aec8f8d837b1d9d3e53b71c20ee1720ff
gorohoroh <gorohoroh@gmail.com>
2019-01-16 23:29:08 +0300

    Extracting SearchResults to a component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Playlist from './components/Playlist/Playlist';
+import SearchResults from "./components/SearchResults/SearchResults";
 
 class App extends Component {
   render() {
@@ -11,46 +12,7 @@ class App extends Component {
             <a>SEARCH</a>
           </div>
           <div className="App-playlist">
-            <div className="SearchResults">
-              <h2>Results</h2>
-              <div className="TrackList">
-                <div className="Track">
-                  <div className="Track-information">
-                    <h3>Tiny Dancer</h3>
-                    <p>Elton John | Madman Across The Water</p>
-                  </div>
-                  <a className="Track-action">+</a>
-                </div>
-                <div className="Track">
-                  <div className="Track-information">
-                    <h3>Tiny Dancer</h3>
-                    <p>Tim McGraw | Love Story</p>
-                  </div>
-                  <a className="Track-action">+</a>
-                </div>
-                <div className="Track">
-                  <div className="Track-information">
-                    <h3>Tiny Dancer</h3>
-                    <p>Rockabye Baby! | Lullaby Renditions of Elton John</p>
-                  </div>
-                  <a className="Track-action">+</a>
-                </div>
-                <div className="Track">
-                  <div className="Track-information">
-                    <h3>Tiny Dancer</h3>
-                    <p>The White Raven | Tiny Dancer</p>
-                  </div>
-                  <a className="Track-action">+</a>
-                </div>
-                <div className="Track">
-                  <div className="Track-information">
-                    <h3>Tiny Dancer - Live Album Version</h3>
-                    <p>Ben Folds | Ben Folds Live</p>
-                  </div>
-                  <a className="Track-action">+</a>
-                </div>
-              </div>
-            </div>
+            <SearchResults/>
             <Playlist/>
           </div>
         </div>
