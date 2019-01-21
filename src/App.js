cf7a78291fb646a9d6e22c10b51409caeeaa3268
gorohoroh <gorohoroh@gmail.com>
2019-01-21 12:58:02 +0300

    Stub for login button and logged in state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import SearchResults from "./components/SearchResults/SearchResults";
 import SearchBar from "./components/SearchBar/SearchBar";
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false
+        };
+        this.authenticate = this.authenticate.bind(this);
+    }
   render() {
     return (
         <div className="App">
           <h1>Ja<span className="highlight">mmm</span>ing</h1>
-          <SearchBar/>
+            { this.state.loggedIn ?  <SearchBar/> : <div className="SearchBar">
+                <button onClick={this.authenticate}>Log in with Spotify</button>
+            </div>}
           <div className="App-playlist">
             <SearchResults/>
             <Playlist/>
@@ -16,6 +25,10 @@ class App extends React.Component {
         </div>
     );
   }
+
+    authenticate() {
+        if (this.state.loggedIn === false) this.setState({loggedIn : true});
+    }
 }
 
 export default App;
