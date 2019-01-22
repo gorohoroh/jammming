a875c001ea7f0be09b2998d2279f7bef65f90150
gorohoroh <gorohoroh@gmail.com>
2019-01-22 13:37:49 +0300

    TrackList: checking to see if search results are available (to avoid calling .map on undefined)

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -5,11 +5,15 @@ import Track from './../Track/Track'
 class TrackList extends React.Component {
     render() {
         return <div className="TrackList">
-            {this.props.searchResults.tracks.map(track =>
-                <Track key={track.id}
-                       artist={track.artist}
-                       album={track.album}
-                       name={track.name}/>)}
+            {
+                !this.props.searchResults ?
+                    <span>No tracks available</span> :
+                    this.props.searchResults.tracks.map(track =>
+                        <Track key={track.id}
+                               artist={track.artist}
+                               album={track.album}
+                               name={track.name}/>)
+            }
         </div>;
     }
 }
