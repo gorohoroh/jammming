e0dfa29c0791ea19a9f4d3677dc65b5ec6a8054b
gorohoroh <gorohoroh@gmail.com>
2019-01-22 14:12:59 +0300

    App: formatting fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -63,23 +63,24 @@ class App extends React.Component {
         };
         this.authenticate = this.authenticate.bind(this);
     }
-  render() {
-    return (
-        <div className="App">
-          <h1>Ja<span className="highlight">mmm</span>ing</h1>
-            { this.state.loggedIn ?  <SearchBar/> : <div className="SearchBar">
-                <button onClick={this.authenticate}>Log in with Spotify</button>
-            </div>}
-          <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults}/>
-            <Playlist playlist={this.state.playlist}/>
-          </div>
-        </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Ja<span className="highlight">mmm</span>ing</h1>
+                {this.state.loggedIn ? <SearchBar/> : <div className="SearchBar">
+                    <button onClick={this.authenticate}>Log in with Spotify</button>
+                </div>}
+                <div className="App-playlist">
+                    <SearchResults searchResults={this.state.searchResults}/>
+                    <Playlist playlist={this.state.playlist}/>
+                </div>
+            </div>
+        );
+    }
 
     authenticate() {
-        if (this.state.loggedIn === false) this.setState({loggedIn : true});
+        if (this.state.loggedIn === false) this.setState({loggedIn: true});
     }
 }
 
