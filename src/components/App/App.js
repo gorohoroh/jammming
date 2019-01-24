9a3bbe8458755109e8f0a12b652683a82a728d2d
gorohoroh <gorohoroh@gmail.com>
2019-01-24 20:37:23 +0300

    Stub for search bar state handling. Looks like I lost the fucking plot here

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -66,6 +66,7 @@ class App extends React.Component {
         this.removeTrack = this.removeTrack.bind(this);
         this.updatePlaylistName = this.updatePlaylistName.bind(this);
         this.savePlaylist = this.savePlaylist.bind(this);
+        this.search = this.search.bind(this);
     }
 
     addTrack(track) {
@@ -98,13 +99,20 @@ class App extends React.Component {
         })
     }
 
+    search(term) {
+        console.log(term)
+    }
+
     render() {
         return (
             <div className="App">
                 <h1>Ja<span className="highlight">mmm</span>ing</h1>
-                {this.state.loggedIn ? <SearchBar/> : <div className="SearchBar">
-                    <button onClick={this.authenticate}>Log in with Spotify</button>
-                </div>}
+                {!this.state.loggedIn ?
+                    <div className="SearchBar">
+                        <button onClick={this.authenticate}>Log in with Spotify</button>
+                    </div> :
+                    <SearchBar onSearch={this.search}/>
+                }
                 <div className="App-playlist">
                     <SearchResults searchResults={this.state.searchResults}
                                    onAdd={this.addTrack}/>
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,11 +2,27 @@ import React from "react";
 import './SearchBar.css'
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
     render() {
         return <div className="SearchBar">
-            <input placeholder="Enter A Song Title"/>
+            <input placeholder="Enter A Song Title"
+                   onChange={this.handleTermChange}/>
             <a>SEARCH</a>
         </div>;
+    }
+
+    search() {
+        // TODO. Step 69. In SearchBar.js, create a method called search that passes the state of the term to this.props.onSearch.
+    }
+
+    handleTermChange(e) {
+        // TODO. Step 71:
+        // * Accepts an event argument
+        // * Sets the state of the search bar's term to the event target's value. (HOW?)
     }
 }
 
